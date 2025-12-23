05fe304421a35b506c2fba0145179bfbb7430b1d
Sahelanthropus <swarnavrdr2@gmail.com>
2025-12-23 13:40:12 +0530

    7 AI providers added

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,22 +1,49 @@
 import { inngest } from "./client";
 import prisma from "@/lib/db";
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world"},
-  { event: "test/hello.world" },
+import {createGoogleGenerativeAI} from "@ai-sdk/google";
+import {generateText} from "ai";
+import{createOpenAI} from "@ai-sdk/openai";
+import{createAnthropic} from "@ai-sdk/anthropic";
+const google=createGoogleGenerativeAI();
+const openai=createOpenAI();
+const anthropic=createAnthropic();
+export const execute = inngest.createFunction(
+  { id: "execute-ai"},
+  { event: "execute/ai" },
   async ({ event, step }) => {
-    await step.sleep("fetching", "5s");
-    await step.sleep("transcribing", "5s");
-    await step.sleep("sending-to-ai", "5s");
+    await step.sleep("pretend","5s");
+    const {steps :geminiSteps} =await step.ai.wrap("gemini-generate-text",
+      generateText,{
+        model:google("gemini-2.5-flash"),
+        system:"You are a helpful assistant",
+        prompt:"what is 2+2?",
+      }
+    );
+    const { steps: openaiSteps } = await step.ai.wrap(
+  "openai-generate-text",
+  generateText,
+  {
+    model: openai("gpt-4"),
+    system: "You are a helpful assistant",
+    prompt: "What is 2+2?",
+  }
+);
+const { steps: anthropicSteps } = await step.ai.wrap(
+  "anthropic-generate-text",
+  generateText,
+  {
+    model: anthropic("claude-sonnet-4-5"),
+    system: "You are a helpful assistant",
+    prompt: "What is 2+2?",
+  }
+);
+   
     
 
-    await step.run("create-workflow",()=>{
-        return prisma.workflow.create({
-            data:{
-                name:"test-workflow"
-
-            },
-        });
-    });
-    return { message: `Hello ${event.data.email}!` };
+    return {
+      geminiSteps,
+      openaiSteps,
+      anthropicSteps,
+    };
   },
 );
