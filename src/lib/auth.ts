98d7c4eef12ce348211f96d273c795b991a4151d
Sahelanthropus <swarnavrdr2@gmail.com>
2025-12-25 18:03:31 +0530

    10 payment integration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,8 @@
 import {betterAuth} from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db";
-
+import { polar, checkout, portal} from "@polar-sh/better-auth";
+import {polarClient} from "./polar";
 export const auth=betterAuth({
     database: prismaAdapter(prisma,{
         provider:"postgresql",
@@ -10,6 +11,26 @@ export const auth=betterAuth({
         enabled: true,
         autoSignIn:true,
     },
+    plugins: [
+        polar({
+            client:polarClient,
+            createCustomerOnSignUp:true,
+            use: [
+                checkout({
+                    products: [
+                        {
+                            productId:"d3dc29a8-362d-424f-ad25-666e13a9fa78",
+                            slug:"pro",
+                        }
+                    ],
+                    successUrl: process.env.POLAR_SUCCESS_URL,
+                    authenticatedUsersOnly:true,
+
+                }),
+                portal()
+            ],
+        })
+    ]
 
 
 
