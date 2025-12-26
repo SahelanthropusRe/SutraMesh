6d6fdb10f7658c55dce005cff8f7043e8713ee3e
Sahelanthropus <swarnavrdr2@gmail.com>
2025-12-26 12:18:37 +0530

    11 workflows crud

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,7 +1,25 @@
+import {prefetchWorkflows} from "@/features/workflows/server/prefetch";
+import {HydrateClient} from "@/trpc/server";
 import { requireAuth } from "@/lib/auth-utils";
-
+import {ErrorBoundary} from "react-error-boundary";
+import {Suspense} from "react";
+import {WorkflowsList,WorkflowsContainer} from "@/features/workflows/components/workflows";
 const Page = async () => {
   await requireAuth();
+  prefetchWorkflows();
+  return (
+    <WorkflowsContainer>
+    <HydrateClient>
+      <ErrorBoundary fallback={<p>Error!</p>}>
+      <Suspense fallback={<p>Loading...</p>}>
+      <WorkflowsList/>
+      </Suspense>
+      </ErrorBoundary>
+
+
+    </HydrateClient>
+    </WorkflowsContainer>
+  )
 
   return <p>Workflows</p>;
 };
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,34 +1,15 @@
-import {baseProcedure,
+import {
   createTRPCRouter,
-  protectedProcedure,
-  premiumProcedure,
+ 
 } from "../init";
+import {workflowsRouter} from "@/features/workflows/server/routers";
 import prisma from "@/lib/db";
 import {inngest} from "@/inngest/client";
 import {TRPCError} from "@trpc/server";
 
 
 export const appRouter = createTRPCRouter({
-  testAi:premiumProcedure.mutation(async ()=>{
-    
-   await inngest.send({
-   name:"execute/ai"});
-
-return {success:true,message:"Job Queued"}
-
-  }),
-  getWorkflows: protectedProcedure.query(({ ctx }) => {
-    return prisma.workflow.findMany();
-  }),
-  createWorkflow: protectedProcedure.mutation(async ()=>{
-   await inngest.send({
-    name:"test/hello.world",
-    data:{
-        email:"swarnavqwe@gmail.com",
-    },
-   })
-   return {success:true,message:"Job Queued"}
-  }),
+  workflows:workflowsRouter,
 });
 
 // export type definition of API
