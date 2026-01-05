3c7607ede70a28c7d8f43e4dc9de863d52e9144b
Sahelanthropus <swarnavrdr2@gmail.com>
2026-01-05 17:41:27 +0530

    19 node variable separation

--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -8,6 +8,7 @@ import { HttpRequestDialog } from "./dialog";
 import { HttpRequestFormValues } from "./dialog";
 
 type HttpRequestNodeData = {
+  variableName?: string;
   endpoint?: string;
   method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
   body?: string;
