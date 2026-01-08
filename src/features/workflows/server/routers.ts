7cedcef9820342e1514186e87a46e09ae9e393a7
Sahelanthropus <swarnavrdr2@gmail.com>
2026-01-08 14:08:34 +0530

    22 google form trigger

--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -6,6 +6,7 @@ import { createTRPCRouter,premiumProcedure, protectedProcedure } from "@/trpc/in
 import {PAGINATION} from "@/config/constants";
 import {NodeType} from "@/generated/prisma";
 import type{Node,Edge} from "@xyflow/react";
+import { sendWorkflowExecution } from "@/inngest/utils";
 export const workflowsRouter = createTRPCRouter({
   execute: protectedProcedure
       .input(z.object({id: z.string()}))
@@ -16,10 +17,9 @@ export const workflowsRouter = createTRPCRouter({
             userId: ctx.auth.user.id,
           },
         });
-        await inngest.send({
-          name:"workflows/execute.workflow",
-          data: {workflowId: input.id},
-          
+        
+        await sendWorkflowExecution({
+          workflowId: input.id,
         });
 
 
