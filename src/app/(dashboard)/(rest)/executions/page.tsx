e5d38e97487c1ba23f5d938fcc4b6e0afbe95e59
Sahelanthropus <swarnavrdr2@gmail.com>
2026-01-14 12:36:22 +0530

    27 execution history

--- src/app/(dashboard)/(rest)/executions/page.tsx
+++ src/app/(dashboard)/(rest)/executions/page.tsx
@@ -1,9 +1,37 @@
 import { requireAuth } from "@/lib/auth-utils";
+import { prefetchCredentials } from "@/features/credentials/server/prefetch";
+import { SearchParams } from "nuqs";
+import { HydrateClient } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import { CredentialsContainer, CredentialsError, CredentialsList, CredentialsLoading } from "@/features/credentials/components/credentials";
+import { credentialsParamsLoader } from "@/features/credentials/server/params-loader";
+import { executionsParamsLoader } from "@/features/executions/server/params-loader";
+import { prefetchExecutions } from "@/features/executions/server/prefetch";
+import { ExecutionsContainer, ExecutionsError, ExecutionsList, ExecutionsLoading } from "@/features/executions/components/executions";
+type Props = {
+  searchParams: Promise<SearchParams>;
+};
 
-const Page = async () => {
+const Page = async ({searchParams}:Props) => {
   await requireAuth();
 
-  return <p>Executions</p>;
+  const params=await executionsParamsLoader(searchParams);
+  prefetchExecutions(params);
+  
+  return (
+    <ExecutionsContainer>
+  <HydrateClient>
+    <ErrorBoundary fallback={<ExecutionsError/>}>
+      <Suspense fallback={<ExecutionsLoading/>}>
+        <ExecutionsList/>
+      </Suspense>
+    </ErrorBoundary>
+  </HydrateClient>
+  </ExecutionsContainer>
+);
 };
+  
+
 
 export default Page;
